d28cd09ccd213e1005a189d111dcf44e0cbdacc4
Danny Luu <cluu79@gmail.com>
2018-01-14 22:17:13 -0600

    added adapter method

--- app/src/main/java/com/example/semidevil/eshoponline/Model/Category.kt
+++ app/src/main/java/com/example/semidevil/eshoponline/Model/Category.kt
@@ -3,4 +3,8 @@ package com.example.semidevil.eshoponline.Model
 /**
  * Created by SemiDevil on 1/13/2018.
  */
-class Category ( val title: String, val image: String )
+class Category ( val title: String, val image: String ){
+    override fun toString(): String {
+        return title
+    }
+}
